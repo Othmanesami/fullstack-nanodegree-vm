8361b0c3fa02f990fe4af682acb481cead1b0919
Othmane Sami <othmane.sami@finamaze.com>
2020-08-11 11:10:43 +0100

    adding post method

--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -1,5 +1,5 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-
+import cgi
 
 
 class webserverHandler(BaseHTTPRequestHandler):
@@ -24,8 +24,12 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
 
-                output = ''
-                output += '<html><body>&#161Holla <a href = "/hello">back to hello page</a></body></html>'
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Hello!</h1>"
+                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += "</body></html>"
+
 
                 self.wfile.write(output)
                 print(output)
@@ -35,6 +39,35 @@ class webserverHandler(BaseHTTPRequestHandler):
         except IOError:
 
             self.send_error(404, 'file not found %s' % self.path)
+
+    
+    def do_POST(self):
+
+        try:
+            self.send_response(301)
+            self.end_headers()
+
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+
+            output = ""
+            output += "<html><body>"
+            output += " <h2> Okay, how about this: </h2>"
+            output += "<h1> %s </h1>" % messagecontent[0]
+            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            output += "</body></html>"
+            self.wfile.write(output)
+            print(output)
+            return
+
+
+        except: 
+
+            pass
+
+
 
 
 

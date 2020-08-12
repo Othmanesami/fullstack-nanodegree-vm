664a626ddd5f5921512fca1302ecb67c71adfea6
Othmane Sami <othmane.sami@finamaze.com>
2020-08-12 11:47:46 +0100

    create flask app

---
+++ vagrant/project.py
@@ -0,0 +1,29 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine("sqlite:///restaurantmenu.db")
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind= engine)
+session = DBSession()
+
+
+#@app.route('/')
+@app.route('/hello')
+def hello():
+    
+    return "Hello world !"
+
+
+
+
+
+if __name__ == "__main__":
+
+    app.debug = True
+    app.run(host= '0.0.0.0', port= 5000)
